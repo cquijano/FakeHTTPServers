85385a467cda9c2abfdc27fe637104bd066b622d
synap5e <simon@gilbertpinfold.co.nz>
2012-10-17 23:20:30 +1300

    Improved .headers - status codes, per file headers
    .headers can now specify custom http response codes e.g. 404
    .headers can be individual to single pages, as described in the readme
    Fixed absolute GET logging
    Increased logging to describe falling to the next page
    Refractored some code

--- README.md
+++ README.md
@@ -5,13 +5,29 @@ A http server designed for faking responses to network connectivity tests (and w
 
 Files are served in the following priority, falling to the next if the file does not exist:
 
-./web/##/(.\*)\*&lt;path&gt;
-
+./web/##/(.\*)\*/&lt;path&gt;
 ./web/&lt;host&gt;/&lt;path&gt;
-
 ./web/&lt;host&gt;/#.html
-
 ./web/#/&lt;path&gt;
-
 ./web/#/#.html
+
+
+A .headers file defines custom headers for a response e.g.
+Content-Type: text/html; charset=UTF-8
+
+The first line of the .headers file can be a number, if so this is the status code of the response
+
+.headers are read in the following priority:
+./web/<host>/<directories>/.headers
+./web/<host>/.headers
+
+e.g.
+www.google.com/foo/bar.html
+would first try
+./web/www.google.com/foo/.headers
+then
+./web/www.google.com/.headers
+
+This allows single pages on the same vhost to use different status codes by using a folder in place of the page and putting the content in a #.html file inside this folder along with the custom .headers for this page
+
 
--- http.py
+++ http.py
@@ -12,41 +12,80 @@ class CustomHTTPServer(resource.Resource):
     
     def __init__(self, allhosts):
         self.allhosts = allhosts
-    
-    #args: twisted.web.server.Request
-    def render_GET(self, request):
+
+    def getResponsePage(self, request):
         host = request.getRequestHostname().split(' ', 1)[0]
         hostdir = os.path.abspath(os.curdir + "/web" + os.path.abspath("/" + host).replace("C:", ""))
-               
-        print "Requested " + os.path.abspath(hostdir + request.path).replace(os.path.abspath(os.curdir + "/web"), "")
+	urlPath = request.path.replace("http://", "").split("/", 1)[1]               
+
+        print "Requested " + os.path.abspath(hostdir + "/" + urlPath).replace(os.path.abspath(os.curdir + "/web"), "")
         
+        for overpath in allhosts.keys():
+            if request.path.split("/")[-1] == overpath:
+		print "Vhosts overwrite: " + allhosts[overpath]
+                return os.path.abspath(os.curdir + "/web/##/"), allhosts[overpath]
+
         if not os.path.exists(hostdir):
             hostdir = os.path.abspath(os.curdir + "/web/#/")
         
-        path = os.path.abspath(hostdir + "/" + request.path.replace("http://", "").split("/", 1)[1])
+        path = os.path.abspath(hostdir + "/" + urlPath)
+	print urlPath
+	print path
         if os.path.isdir(path):
-            path = os.path.abspath(path + root_path)
+	    print path + " is a directory"
+            path = os.path.abspath(path + "/" + root_path)
+	    print "Falling to : " + path
         if not os.path.exists(path):
-            path = os.path.abspath(os.path.dirname(hostdir + request.path) + path_404)
+	    print path + " does not exist"
+            path = os.path.abspath(hostdir + "/" + urlPath + "/" + path_404)
+	    print "Falling to: " + path
         if not os.path.exists(path):
+	    print path + " does not exist"
             path = os.path.abspath(os.curdir + "/web/#" + path_404)
+	    print "Falling to: " + path
             
-        for overpath in allhosts.keys():
-            if request.path.split("/")[-1] == overpath:
-                path = allhosts[overpath]
-                break
             
         print "Serving " + path.replace(os.path.abspath(os.curdir + "/web"), "")
+	return hostdir, path
+
+    def getHeadersFile(self, hostdir, page):
+	path = os.path.split(page)[0] + "/.headers"
+	print "Headers: " + path
+	if os.path.exists(path):
+	    return path
+	
+	path = hostdir + "/.headers"
+	print "Does not exist, falling to: " + path
+	if os.path.exists(path):
+	    return path
+	
+	print "Does not exist, not using custom headers"
+	return None
+
+    
+    #args: twisted.web.server.Request
+    def render_GET(self, request):
+
+	hostdir, page = self.getResponsePage(request)
         
-        if os.path.exists(hostdir + "/.headers"):
-            f = file(hostdir + "/.headers")
-            for line in f.read().split("\n"):
+	headersFile = self.getHeadersFile(hostdir, page)
+
+        if headersFile:
+	    print "Overwriting headers with: " + headersFile
+            f = file(headersFile)
+	    fileData = f.read()
+	    fileLines = fileData.split("\n")
+	    if fileLines[0].isdigit() == True:
+		request.setResponseCode(int(fileLines[0]))
+	    else:
+		fileLines = fileLines[1:]
+            for line in fileLines:
                 try:
                     request.setHeader(line.split(": ")[0], line.split(": ")[1])
                 except IndexError:
                     pass
         
-        f = file(path)
+        f = file(page)
         return f.read()
         f.close()
 
