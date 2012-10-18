21c66b38494961d53351f63570c798aa85373395
synap5e <github@gilbertpinfold.co.nz>
2012-10-18 18:46:34 +1300

    Allowed for carriage return in the headers file
    Also fixed some tabulation to use spaces all throughout the file

--- http.py
+++ http.py
@@ -16,74 +16,76 @@ class CustomHTTPServer(resource.Resource):
     def getResponsePage(self, request):
         host = request.getRequestHostname().split(' ', 1)[0]
         hostdir = os.path.abspath(os.curdir + "/web" + os.path.abspath("/" + host).replace("C:", ""))
-	urlPath = request.path.replace("http://", "").split("/", 1)[1]               
+        urlPath = request.path.replace("http://", "").split("/", 1)[1]               
 
         print "Requested " + os.path.abspath(hostdir + "/" + urlPath).replace(os.path.abspath(os.curdir + "/web"), "")
         
         for overpath in allhosts.keys():
             if request.path.split("/")[-1] == overpath:
-		print "Vhosts overwrite: " + allhosts[overpath]
+                print "Vhosts overwrite: " + allhosts[overpath]
                 return os.path.abspath(os.curdir + "/web/##/"), allhosts[overpath]
 
         if not os.path.exists(hostdir):
             hostdir = os.path.abspath(os.curdir + "/web/#/")
         
         path = os.path.abspath(hostdir + "/" + urlPath)
-	print urlPath
-	print path
+        print urlPath
+        print path
         if os.path.isdir(path):
-	    print path + " is a directory"
+            print path + " is a directory"
             path = os.path.abspath(path + "/" + root_path)
-	    print "Falling to : " + path
+        print "Falling to : " + path
         if not os.path.exists(path):
-	    print path + " does not exist"
+            print path + " does not exist"
             path = os.path.abspath(hostdir + "/" + urlPath + "/" + path_404)
-	    print "Falling to: " + path
+        print "Falling to: " + path
         if not os.path.exists(path):
-	    print path + " does not exist"
+            print path + " does not exist"
             path = os.path.abspath(os.curdir + "/web/#" + path_404)
-	    print "Falling to: " + path
+        print "Falling to: " + path
             
             
         print "Serving " + path.replace(os.path.abspath(os.curdir + "/web"), "")
-	return hostdir, path
+        return hostdir, path
 
     def getHeadersFile(self, hostdir, page):
-	path = os.path.split(page)[0] + "/.headers"
-	print "Headers: " + path
-	if os.path.exists(path):
-	    return path
-	
-	path = hostdir + "/.headers"
-	print "Does not exist, falling to: " + path
-	if os.path.exists(path):
-	    return path
-	
-	print "Does not exist, not using custom headers"
-	return None
+        path = os.path.split(page)[0] + "/.headers"
+        print "Headers: " + path
+        if os.path.exists(path):
+            return path
+    
+        path = hostdir + "/.headers"
+        print "Does not exist, falling to: " + path
+        if os.path.exists(path):
+            return path
+    
+        print "Does not exist, not using custom headers"
+        return None
 
     
     #args: twisted.web.server.Request
     def render_GET(self, request):
 
-	hostdir, page = self.getResponsePage(request)
-        
-	headersFile = self.getHeadersFile(hostdir, page)
+        hostdir, page = self.getResponsePage(request)
+            
+        headersFile = self.getHeadersFile(hostdir, page)
 
         if headersFile:
-	    print "Overwriting headers with: " + headersFile
+            print "Overwriting headers with: " + headersFile
             f = file(headersFile)
-	    fileData = f.read()
-	    fileLines = fileData.split("\n")
-	    if fileLines[0].isdigit() == True:
-		request.setResponseCode(int(fileLines[0]))
-	    else:
-		fileLines = fileLines[1:]
-            for line in fileLines:
-                try:
-                    request.setHeader(line.split(": ")[0], line.split(": ")[1])
-                except IndexError:
-                    pass
+        fileData = f.read()
+        fileLines = fileData.split("\n")
+        if fileLines[0].rstrip().isdigit() == True:
+            print "Replacing status code"
+            request.setResponseCode(int(fileLines[0]))
+        else:
+            fileLines = fileLines[1:]
+        for line in fileLines:
+            try:
+                rsline = line.rstrip()
+                request.setHeader(rsline.split(": ")[0], rsline.split(": ")[1])
+            except IndexError:
+                pass
         
         f = file(page)
         return f.read()
